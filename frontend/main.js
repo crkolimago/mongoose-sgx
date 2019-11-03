b69fa188ec75a7c6d801ab41861733f46d32ae54
crkolimago <crkolimago@gmail.com>
2019-11-03 15:56:45 -0500

    get and post working with axios

--- frontend/main.js
+++ frontend/main.js
@@ -1,53 +1,60 @@
-function performGetRequest1() {
-    var resultElement = document.getElementById('getResult1');
-    resultElement.innerHTML = '';
-    
-    axios.get('http://jsonplaceholder.typicode.com/todos')
-      .then(function (response) {
-        resultElement.innerHTML = generateSuccessHTMLOutput(response);
-      })
-      .catch(function (error) {
-        resultElement.innerHTML = generateErrorHTMLOutput(error);
-      });   
-  }
+function performGetRequest() {
+  var resultElement = document.getElementById('getResult');
+  resultElement.innerHTML = '';
 
-  function generateSuccessHTMLOutput(response) {
-    return  '<h4>Result</h4>' + 
-            '<h5>Status:</h5> ' + 
-            '<pre>' + response.status + ' ' + response.statusText + '</pre>' +
-            '<h5>Headers:</h5>' + 
-            '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' + 
-            '<h5>Data:</h5>' + 
-            '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>'; 
-  }function generateErrorHTMLOutput(error) {
-    return  '<h4>Result</h4>' + 
-            '<h5>Message:</h5> ' + 
-            '<pre>' + error.message + '</pre>' +
-            '<h5>Status:</h5> ' + 
-            '<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>' +
-            '<h5>Headers:</h5>' + 
-            '<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>' + 
-            '<h5>Data:</h5>' + 
-            '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>'; 
-  }
-
-  function performGetRequest2() {
-    var resultElement = document.getElementById('getResult2');
-    var todoId = document.getElementById('todoId').value;
-    resultElement.innerHTML = '';
-    
-    axios.get('http://jsonplaceholder.typicode.com/todos', {
-      params: {
-        id: todoId
-      }
-    })
+  axios.get('http://localhost:8000/')
     .then(function (response) {
-      console.log(response);
       resultElement.innerHTML = generateSuccessHTMLOutput(response);
     })
     .catch(function (error) {
-        resultElement.innerHTML = generateErrorHTMLOutput(error);
+      resultElement.innerHTML = generateErrorHTMLOutput(error);
     });
-  }
+}
 
-  
+document.getElementById('todoInputForm').addEventListener('submit', performPostRequest); function performPostRequest(e) {
+  var resultElement = document.getElementById('postResult');
+  var todoTitle = document.getElementById('todoTitle').value;
+  resultElement.innerHTML = '';
+
+  axios.post('http://localhost:8000/upload', {
+    data: todoTitle,
+  })
+    .then(function (response) {
+      resultElement.innerHTML = generateSuccessHTMLOutput(response);
+    })
+    .catch(function (error) {
+      resultElement.innerHTML = generateErrorHTMLOutput(error);
+    });
+
+  e.preventDefault();
+}
+
+function generateSuccessHTMLOutput(response) {
+  console.log(response);
+  return '<h4>Result</h4>' +
+    '<h5>Status:</h5> ' +
+    '<pre>' + response.status + ' ' + response.statusText + '</pre>' +
+    '<h5>Headers:</h5>' +
+    '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
+    '<h5>Data:</h5>' +
+    '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
+}
+
+function generateErrorHTMLOutput(error) {
+  return '<h4>Result</h4>' +
+    '<h5>Message:</h5> ' +
+    '<pre>' + error.message + '</pre>' +
+    '<h5>Status:</h5> ' +
+    '<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>' +
+    '<h5>Headers:</h5>' +
+    '<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>' +
+    '<h5>Data:</h5>' +
+    '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>';
+}
+
+function clearOutput() {
+  var resultElement = document.getElementById('getResult');
+  resultElement.innerHTML = '';
+  var resultElement = document.getElementById('postResult');
+  resultElement.innerHTML = '';
+}
